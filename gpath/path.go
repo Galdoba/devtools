4c158a967c6cecc12ba3596c30e76f4334dc9ddb
Galdoba <galdoba@mail.ru>
2022-04-05 13:43:25 +1000

    path cleaner

--- gpath/path.go
+++ gpath/path.go
@@ -1,7 +1,10 @@
 package gpath
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
@@ -18,22 +21,97 @@ const (
 	DataPath
 )
 
-type path struct {
-	input     string
-	server    string
-	tome      string
-	dir       []string
-	fileName  string
-	extention string
-	osyst     string
-	pathType  int
+type pathStr struct {
+	input      string
+	executable string
+	computer   string
+	tome       string
+	dir        []string
+	fileName   string
+	extention  string
+	osyst      string
+	pathType   int
+	isDir      bool
+	isFile     bool
+	isAbs      bool
 }
 
-func newPath(input string, pathType ...int) *path {
-	p := path{}
-	p.input = input
-	if !strings.Contains(input, `\\`) {
-		filepath.Abs()
+func newPath(input string) (*pathStr, error) {
+	if strings.TrimSpace(input) == "" {
+		return nil, fmt.Errorf("path was not specified")
 	}
-	return &p
+	abs, err := filepath.Abs(input)
+	if err != nil {
+		return nil, err
+	}
+	pathStats, errStat := os.Stat(input)
+	if errStat != nil {
+		return nil, fmt.Errorf(errStat.Error()+" (%v)", abs)
+	}
+	p := pathStr{}
+
+	p.input = input
+	if abs == input {
+		p.isAbs = true
+	}
+	p.isDir = pathStats.Mode().IsDir()
+	p.isFile = pathStats.Mode().IsRegular()
+	//p.computer, _ = os.Hostname()
+	p.executable, _ = os.Executable()
+	p.osyst = runtime.GOOS
+	input = filepath.ToSlash(input)
+	data := strings.Split(input, "/")
+	p.tome = filepath.VolumeName(input)
+	if p.isDir {
+		p.extention = "_"
+	}
+	if strings.Contains(input, "/") {
+		comp := strings.Split(input, "/")
+		compName := strings.Split(comp[1], "/")
+		p.computer = compName[0]
+	}
+
+	switch filepath.Base(input) {
+	case "":
+		p.fileName = "---"
+
+	default:
+		p.fileName = filepath.Base(input)
+	}
+	fn := strings.Split(p.fileName, ".")
+	if len(fn) > 1 {
+		p.extention = fn[len(fn)-1]
+	}
+	for _, dr := range data {
+		if dr == "" || dr == p.computer || dr == p.fileName {
+			continue
+		}
+
+		p.dir = append(p.dir, dr)
+	}
+	// if p.computer == "" {
+	// 	return &p, fmt.Errorf("computer is not set [%v]", input)
+	// }
+	if p.tome == "" {
+		return &p, fmt.Errorf("tome is not set [%v]", input)
+	}
+	if p.dir == nil {
+		return &p, fmt.Errorf("dir is not set [%v]", input)
+	}
+	if p.fileName == "" {
+		return &p, fmt.Errorf("fileName is not set [%v]", input)
+	}
+	if p.extention == "" {
+		return &p, fmt.Errorf("extention is not set [%v]", input)
+	}
+	if p.osyst == "" {
+		return &p, fmt.Errorf("osyst is not set [%v]", input)
+	}
+	// if p.pathType == UNSPECIFIED {
+	// 	return &p, fmt.Errorf("pathType is not set [%v]", input)
+	// }
+	// if p.pathType != SourcePath && p.pathType != DestinationPath && p.pathType != DataPath && p.pathType != UNSPECIFIED {
+	// 	return &p, fmt.Errorf("pathType is unknown [%v]", input)
+	// }
+	return &p, nil
 }
--- gpath/path_test.go
+++ gpath/path_test.go
@@ -1,29 +1,46 @@
 package gpath
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func pathInput() []string {
 	return []string{
 		"input",
 		`c:\Users\pemaltynov\go\src\github.com\Galdoba\devtools\gpath\path.go`,
 		`d:\MUX\MUX_2022-03-21\`,
+		`.\MUX\`,
 		"d:\\MUX\\MUX_2022-03-25\\",
+		"\\\\192.168.31.4\\root\\EDIT\\@trailers_temp\\Last_Survivors_TRL_AUDIORUS20.m4a",
+		``,
+		"",
+		".",
+		"d:\\IN\\IN_2022-03-28\\LINK_to_File.lnk",
+		"d:\\IN\\IN_2022-03-28\\LINK_to_Dir.lnk",
 	}
 }
 
 func TestPath(t *testing.T) {
 	for num, input := range pathInput() {
 		n := num + 1
-		p := newPath(input)
+		fmt.Println("Start test: " + input)
+		p, err := newPath(input)
+		if err != nil {
+			t.Errorf("internal error: %v", err.Error())
+			fmt.Println(" ")
+			continue
+		}
 		if p == nil {
 			t.Errorf("path is <nil>")
+			continue
 		}
 		if p.input == "" {
 			t.Errorf("Test %v:\ninput: '%v'\n	p.input is not set", n, input)
 		}
-		if p.server == "" {
-			t.Errorf("Test %v:\ninput: '%v'\n	p.server is not set", n, input)
-		}
+		// if p.computer == "" {
+		// 	t.Errorf("Test %v:\ninput: '%v'\n	p.computer is not set", n, input)
+		// }
 		if p.tome == "" {
 			t.Errorf("Test %v:\ninput: '%v'\n	p.tome is not set", n, input)
 		}
@@ -39,11 +56,14 @@ func TestPath(t *testing.T) {
 		if p.osyst == "" {
 			t.Errorf("Test %v:\ninput: '%v'\n	p.osyst is not set", n, input)
 		}
-		if p.pathType == UNSPECIFIED {
-			t.Errorf("Test %v:\ninput: '%v'\n	p.pathType is not set", n, input)
-		}
-		if p.pathType != SourcePath && p.pathType != DestinationPath && p.pathType != DataPath && p.pathType != UNSPECIFIED {
-			t.Errorf("Test %v:\ninput: '%v'\n	p.pathType is unknown", n, input)
-		}
+		// if p.pathType == UNSPECIFIED {
+		// 	t.Errorf("Test %v:\ninput: '%v'\n	p.pathType is not set", n, input)
+		// }
+		// if p.pathType != SourcePath && p.pathType != DestinationPath && p.pathType != DataPath && p.pathType != UNSPECIFIED {
+		// 	t.Errorf("Test %v:\ninput: '%v'\n	p.pathType is unknown", n, input)
+		// }
+		fmt.Println("SUCCESS!!")
+		//fmt.Println(p)
+		fmt.Println(" ")
 	}
 }
