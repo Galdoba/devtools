7312a42306137b4867cee1ed9d46d0fd35b81bad
Galdoba <galdoba@mail.ru>
2023-02-03 18:09:17 +1000

    catcher

--- parser/ebnf/ebnfReader.go
+++ parser/ebnf/ebnfReader.go
@@ -24,19 +24,6 @@ func OmitComments(data string) string {
 	return data
 }
 
-func MapTerminalStrings(data string) map[int]string {
-	tsm := make(map[int]string)
-	currentTS := -1
-	feed := strings.Split(data, "")
-	for i, f := range feed {
-		if 
-		tail := strings.Join(feed[i:])
-	}
-	bod, pre, suf := trimClosingsGreedy(data, `"`, `"`)
-
-	return tsm
-}
-
 func debracketGreedy(str, open, close string) string {
 	//111(*222*)333
 	feed := strings.Split(str, "")
@@ -98,39 +85,159 @@ func haveTerminalString(data string) bool {
 /*
 Usage					Notation	name	priority
 ----------------		---------	------	----
-definition				=			LIT		4
-concatenation			,			JOIN	5
-termination				;
-alternation				|			OR		3
-optional				[ ... ]		OPT		1
-repetition				{ ... }		WHILE	2
-grouping				( ... )
-terminal string			" ... "
-terminal string			' ... '
-comment					(* ... *)
-special sequence		? ... ?
-exception				-			NOT		0
+0   definition				=			LIT		4
+1   concatenation			,			JOIN	5
+2   termination				;
+3   alternation				|			OR		3
+4   optional				[ ... ]		OPT		1
+5   repetition				{ ... }		WHILE	2
+6   grouping				( ... )
+7   terminal string			" ... "
+8   terminal string			' ... '
+9   comment					(* ... *)	_OMIT_
+10  special sequence		? ... ? 	_OMIT_
+11  exception				-			NOT		0
 /////////////////////////////
 любая строка для обработки должна начинаться с одного из Notation
 если оно не найдено - ошибка некоректный синтаксис или глиф не определен
+0 ищем определение ( = ) 																	возвращаем Trigger, LHS, RHS
+1 ищем следующий простой триггер ( = , ; |) 												возвращаем Trigger, RHS, rest
+2 ищем следующий закрытый триггер ( [ .. ] { .. } ( .. ) " .. " ' .. ' (* .. *) ? .. ? ) 	возвращаем Trigger, RHS, rest
+3 ищем отрицание ( - ) 																		возвращаем Trigger, RHS, rest
 */
-func nextToken(data string) (token string, RHS string, rest string, err error) {
+
+type token struct {
+	ttype int
+	RHS   string
+	LHS   string
+	tail  string
+	err   error
+}
+
+const (
+	undefined       = iota
+	definition      //  =
+	concatenation   //  ,
+	termination     //  ;
+	alternation     //  |
+	optional        //  [ ... ]
+	repetition      //  { ... }
+	grouping        //  ( ... )
+	terminalstring1 //  " ... "
+	terminalstring2 //  ' ... '
+	comment         //  (* ... *)	- omit or ERROR?
+	specialsequence //  ? ... ?		- omit or ERROR?
+	exception       //  -
+)
+
+var invalidSequances = []string{"(*)", "(:)", "(/)"}
+
+func haveInvalidSeq(data string) string {
+	for _, val := range invalidSequances {
+		if strings.Contains(data, val) {
+			return val
+		}
+	}
+	return ""
+}
+
+func catchToken(data string) token {
+	//TODO: перестроить так чтобы он ловил терминальные символы в порядке из стандарта (таблица 1 стр 7)
+	feed := strings.Split(data, "")
+	t := token{}
+	t.ttype = undefined
+	buf := ""
+	for i, f := range feed {
+		buf += f
+		switch {
+		default:
+			t.err = fmt.Errorf("token not defined")
+		case strings.HasSuffix(buf, "="):
+			t.ttype = definition
+			t.LHS = formatLHS(buf, "=")
+			t.RHS = strings.Join(feed[i:], "")
+		case strings.HasSuffix(buf, ","):
+			t.ttype = concatenation
+			t.LHS = formatLHS(buf, ",")
+			t.RHS = strings.Join(feed[i:], "")
+		case strings.HasSuffix(buf, ";"):
+			t.ttype = termination
+			t.LHS = formatLHS(buf, ";")
+			t.RHS = ""
+		case strings.HasSuffix(buf, "|"):
+			t.ttype = alternation
+			t.LHS = formatLHS(buf, "|")
+			t.RHS = strings.Join(feed[i:], "")
+		case strings.HasSuffix(buf, "["):
+			t.ttype = optional
+			t.LHS = formatLHS(buf, "[")
+			t.RHS, t.tail, t.err = grabBrackets(strings.Join(feed[i:], ""), "[", "]")
+			return t
+		case strings.HasSuffix(buf, "{"):
+			t.ttype = repetition
+			t.LHS = formatLHS(buf, "{")
+			t.RHS, t.tail, t.err = grabBrackets(strings.Join(feed[i:], ""), "{", "}")
+			return t
+		case strings.HasSuffix(buf, "("):
+			t.ttype = grouping
+			t.LHS = formatLHS(buf, "(")
+			t.RHS, t.tail, t.err = grabBrackets(strings.Join(feed[i:], ""), "(", ")")
+			return t
+		case strings.HasSuffix(buf, `"`):
+			t.ttype = terminalstring1
+			t.LHS, t.tail, t.err = grabGridy(strings.Join(feed[i:], ""), `"`)
+			return t
+		case strings.HasSuffix(buf, "'"):
+			t.ttype = terminalstring2
+			t.LHS, t.tail, t.err = grabGridy(strings.Join(feed[i:], ""), "'")
+			return t
+		case strings.HasSuffix(buf, "*"):
+			t.ttype = terminalstring2
+			t.LHS, t.tail, t.err = grabGridy(strings.Join(feed[i:], ""), "*")
+			return t
+		case strings.HasSuffix(buf, "?"):
+			t.ttype = specialsequence
+			t.LHS, t.tail, t.err = grabGridy(strings.Join(feed[i:], ""), "?")
+			return t
+		}
+		t.LHS += f
+	}
+	return t
+}
+
+func formatLHS(s, suf string) string {
+	s = strings.TrimSuffix(s, suf)
+	s = strings.TrimSpace(s)
+	return s
+}
+
+func grabGridy(data, sep string) (rhs string, rest string, err error) {
+	if !strings.HasPrefix(data, sep) {
+		err = fmt.Errorf("token |%v| is not opened with (%v)", data, sep)
+		rhs = data
+		return
+	}
+	dt := strings.Split(data, sep)
+	if len(dt) < 3 {
+		err = fmt.Errorf("token |%v| is not closed with (%v)", data, sep)
+		rhs = data
+		return
+	}
+	rhs = dt[1]
+	rest = strings.Join(dt[2:], sep)
+	return
+}
+
+func nextToken(data string) (token string, RHS string, LHS string, err error) {
 	//token, RHS, rest := string, string, string
 	data = OmitComments(data)
 	switch {
 	default:
+		err = fmt.Errorf("unrecog")
 		token = "UNRECONIZED"
 		RHS = data
-	case isDefinition(data):
-		token = "LIT"
-		//RHS, rest, err = grabDefinition(data)
-	case isTerminalString1(data):
-		token = "terminal_string_1"
-		RHS, rest, err = grabTS1(data)
-	case isTerminalString2(data):
-		token = "terminal_string_2"
-		RHS, rest, err = grabTS2(data)
 	}
+
 	return
 }
 
@@ -229,15 +336,6 @@ func isException(data string) bool {
 
 */
 
-type tokenList struct {
-	t []token
-}
-
-type token struct {
-	name string
-	rhs  string
-}
-
 //func parseTokens(data string) tokenList {
 
 //}
@@ -281,4 +379,53 @@ func trimPrefixBefore(str, trig string) (string, string) {
 func trimSuffixBefore(str, trig string) (string, string) {
 	arr := strings.Split(str, trig)
 	return arr[len(arr)-1], strings.Join(arr[:len(arr)-1], trig)
+}
+
+func trimFirstRune(s string) string {
+	for i := range s {
+		if i > 0 {
+			return s[i:]
+		}
+	}
+	return ""
+}
+
+func grabBrackets(str string, op, cl string) (capt, rest string, err error) {
+	if op == cl {
+		return str, "", fmt.Errorf("brackets must not match ('%v'='%v')", op, cl)
+	}
+	depth := 0
+	rhs := ""
+	for str != "" {
+		switch {
+		case strings.HasPrefix(str, op):
+			str = strings.TrimPrefix(str, op)
+			depth++
+			if depth != 1 {
+				rhs += op
+				continue
+			}
+		case strings.HasPrefix(str, cl):
+			str = strings.TrimPrefix(str, cl)
+			depth--
+			if depth != 0 {
+				rhs += cl
+				continue
+			}
+			return rhs, str, nil
+		case depth == 0:
+			return "", str, fmt.Errorf("brackets were not opened")
+		}
+		rhs += firstN(str, 1)
+		str = trimFirstRune(str)
+	}
+	return rhs, str, fmt.Errorf("brackets were not closed")
+}
+
+func firstN(s string, n int) string {
+	r := []rune(s)
+	if len(r) > n {
+		return string(r[:n])
+	}
+	return s
 }
--- parser/ebnf/ebnf_test.go
+++ parser/ebnf/ebnf_test.go
@@ -2,20 +2,16 @@ package ebnf
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 )
 
 func parseConstructionInput() []string {
 	return []string{
+		`111?222?333`,
 		`111(*222*)333`,
 		`111(*)222(*333`,
 		`111(*222*)333(*444*)555`,
 		`111(*22*)*6*)333(*444*)555`,
-		"111'222'333",
-		`111"222"333`,
-		"'222'333",
-		`"222"333`,
 		//`111222333`,
 	}
 }
@@ -28,26 +24,21 @@ func parseConstructionInput() []string {
 */
 
 func TestNextToken(t *testing.T) {
-	data1 := `[{'abc'}]def`
-	fmt.Println(nextToken(data1))
-	data2 := `{'abc'}def`
-	fmt.Println(nextToken(data2))
-	data3 := `'abc'def`
-	fmt.Println(nextToken(data3))
-	data4 := `'def'`
-	fmt.Println(nextToken(data4))
+	/*
+		{aaaa{bbb}ccc}ddd
+	*/
+	str := `?'aaa""a'b?b'cc*)cc`
+	//fmt.Println(grabGridy(str, `"`))
+	//op, cl := "(*", "*)"
+	fmt.Println(catchToken(str))
 
-	for i, dt := range strings.Split("baaaab", "b") {
-		fmt.Println(i, dt)
-	}
 }
 
 func TestOmitComments(t *testing.T) {
-	return
 	fmt.Println("OMIT COMMENTS")
 	for _, input := range parseConstructionInput() {
-		fmt.Println("RUN:", input)
-		fmt.Println(OmitComments(input), "--")
+		fmt.Print("RUN:", input)
+		fmt.Println(" ==> ", OmitComments(input))
 	}
 
 }
