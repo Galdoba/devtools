f387e3e3e9442ca3833151b4dc78b6f3b4ff78ae
Galdoba <galdoba@mail.ru>
2023-04-15 20:54:29 +1000

    no ebnf

--- parser/ebnf/ebnf_test.go
+++ parser/ebnf/ebnf_test.go
@@ -1,54 +1,54 @@
 package ebnf
 
-import (
-	"fmt"
-	"testing"
-)
+// import (
+// 	"fmt"
+// 	"testing"
+// )
 
-func parseConstructionInput() []string {
-	return []string{
-		`111?222?333`,
-		`111(*222*)333`,
-		`111(*)222(*333`,
-		`111(*222*)333(*444*)555`,
-		`111(*22*)*6*)333(*444*)555`,
-		//`111222333`,
-	}
-}
+// func parseConstructionInput() []string {
+// 	return []string{
+// 		`111?222?333`,
+// 		`111(*222*)333`,
+// 		`111(*)222(*333`,
+// 		`111(*222*)333(*444*)555`,
+// 		`111(*22*)*6*)333(*444*)555`,
+// 		//`111222333`,
+// 	}
+// }
 
-//(*
-//*)
-/*
-`111
-)222(*333`,
-*/
+// //(*
+// //*)
+// /*
+// `111
+// )222(*333`,
+// */
 
-func TestNextToken(t *testing.T) {
-	/*
-		{aaaa{bbb}ccc}ddd
-	*/
-	str := `?'aaa""a'b?b'cc*)cc`
-	//fmt.Println(grabGridy(str, `"`))
-	//op, cl := "(*", "*)"
-	fmt.Println(catchToken(str))
+// func TestNextToken(t *testing.T) {
+// 	/*
+// 		{aaaa{bbb}ccc}ddd
+// 	*/
+// 	str := `?'aaa""a'b?b'cc*)cc`
+// 	//fmt.Println(grabGridy(str, `"`))
+// 	//op, cl := "(*", "*)"
+// 	fmt.Println(catchToken(str))
 
-}
+// }
 
-func TestOmitComments(t *testing.T) {
-	fmt.Println("OMIT COMMENTS")
-	for _, input := range parseConstructionInput() {
-		fmt.Print("RUN:", input)
-		fmt.Println(" ==> ", OmitComments(input))
-	}
+// func TestOmitComments(t *testing.T) {
+// 	fmt.Println("OMIT COMMENTS")
+// 	for _, input := range parseConstructionInput() {
+// 		fmt.Print("RUN:", input)
+// 		fmt.Println(" ==> ", OmitComments(input))
+// 	}
 
-}
+// }
 
-func TestClosings(t *testing.T) {
-	return
-	fmt.Println("CLOSINGS")
-	for _, input := range parseConstructionInput() {
-		fmt.Println("RUN:", input)
-		fmt.Println(TerminalString(input))
-	}
+// func TestClosings(t *testing.T) {
+// 	return
+// 	fmt.Println("CLOSINGS")
+// 	for _, input := range parseConstructionInput() {
+// 		fmt.Println("RUN:", input)
+// 		fmt.Println(TerminalString(input))
+// 	}
 
-}
+// }
